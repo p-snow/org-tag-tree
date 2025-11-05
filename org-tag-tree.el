2332b7ba9408837662dbe2ec585e879f1c9bef65
p-snow <public@p-snow.org>
2025-11-05 21:39:16 +0900

    Add package commentary

--- org-tag-tree.el
+++ org-tag-tree.el
@@ -24,14 +24,41 @@
 
 ;;; Commentary:
 
-;; tag tree is
+;; By default, Org tag hierarchies are declared with the #+TAGS: syntax:
+
+;; #+TAGS: [GTD : Control Persp]
+;; #+TAGS: [Control : Context Task]
+;; #+TAGS: [Persp : Vision Goal]
+
+;; Or define globally:
+
+;; (setq org-tag-alist
+;;       '((:startgrouptag)("GTD")(:grouptags)("Control")("Persp")(:endgrouptag)
+;;         (:startgrouptag)("Control")(:grouptags)("Context")("Task")(:endgrouptag)
+;;         (:startgrouptag)("Persp")(:grouptags)("Vision")("Goal")(:endgrouptag)))
+
+;; org-tag-tree lets you express the same hierarchy directly in an Org outline
+;; and attach any explanatory notes to nodes:
+
+;; * GTD                    :tag:
+;; ** Control
+;; *** Context
+;;     Used for each context (e.g. @home, @work)
+;; *** Task
+;;     Use for task-level tags
+;; ** Persp
+;; *** Vision
+;;     Long-term, big-picture items
+;; *** Goal
+;;     Concrete outcomes to measure progress
+
+;; This keeps the tag inheritance visible in the document structure and lets you
+;; include descriptions, examples, or usage notes for each tag node.
 
 ;;; Code:
 
 (require 'org)
 (require 'cl-lib)
-
-;;;; Variables
 
 ;;;; Customization
 
